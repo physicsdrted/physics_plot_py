83005b0c53830708cc039b8147c95cff97cdf321
physicsdrted <physicsdrted@gmail.com>
2025-04-10 20:26:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -190,11 +190,10 @@ custom_html = """
 <style>
     .banner {
         width: 100%;
-        height: 300px;
+        height: 200px;
         overflow: visible;
     }
     .banner img {
-        width: 100%;
         object-fit: contain;
     }
 </style>
